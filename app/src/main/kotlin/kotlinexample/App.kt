59581723ed935333774d315b2f5b13b4ce3f4da4
p-haruki.fukuchi <p-haruki.fukuchi@inside.ai>
2022-02-22 12:53:43 +0900

    feat: アプリケーションサービス

--- app/src/main/kotlin/kotlinexample/App.kt
+++ app/src/main/kotlin/kotlinexample/App.kt
@@ -1,10 +1,41 @@
 package kotlinexample
 
-class App {
-  val greeting: String
-    get() { return "Hello World!" }
+import kotlinexample.casting.transfer.CheckingAccountTransferRoleCasting
+import kotlinexample.casting.transfer.SavingAccountTransferRoleCasting
+import kotlinexample.infrastracture.CheckingAccountRepositoryImpl
+import kotlinexample.infrastracture.SavingAccountRepositoryImpl
+import kotlinexample.interaction.transfer.TransferContext
+import kotlinexample.interaction.transfer.TransferInteraction
+import kotlinexample.interaction.transfer.TransferRoleOrder
 
-  operator fun invoke() {
-    println(greeting)
+class App {
+  private val checkingAccountRepository = CheckingAccountRepositoryImpl()
+  private val savingAccountRepository = SavingAccountRepositoryImpl()
+  private val castings = listOf(
+    CheckingAccountTransferRoleCasting(checkingAccountRepository),
+    SavingAccountTransferRoleCasting(savingAccountRepository),
+  )
+
+  interface Command {
+    val sourceOrder: TransferRoleOrder
+    val destinationOrder: TransferRoleOrder
+    val amount: Int
+  }
+
+  operator fun invoke(command: Command) {
+    checkingAccountRepository.log()
+    savingAccountRepository.log()
+    executeTransfer(command)
+    checkingAccountRepository.log()
+    savingAccountRepository.log()
+  }
+
+  private fun executeTransfer(command: Command) {
+    TransferInteraction(TransferContext(castings))
+      .standBy(sourceOrder = command.sourceOrder, destinationOrder = command.destinationOrder)
+      .also { (interact, close) ->
+        interact(command.amount)
+        close()
+      }
   }
 }
